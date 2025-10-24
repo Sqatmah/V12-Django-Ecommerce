c5883ac3cc1a04a0a70b3c3bc319e00c58bddac8
sqatm <sqatmah@gmail.com>
2025-10-24 21:26:44 +0300

    adding about page and isolated the basehtml and navbar with django block

--- store/views.py
+++ store/views.py
@@ -8,3 +8,8 @@ from . models import Product
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products': products})   
+
+
+
+def about(request):
+    return render(request, 'about.html', {})
